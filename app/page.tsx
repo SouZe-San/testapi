891ccb90fdca42757c3e4b1b40afa827188ac5a8
SouZe-San <2003soumyajit@gmial.com>
2023-07-27 14:25:47 +0530

    jh

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
    <main>
     <h2>This is Main Page</h2>
+    <button>
+    <Link href="/login">login</Link>
+    </button>
    </main>
   )
 }
